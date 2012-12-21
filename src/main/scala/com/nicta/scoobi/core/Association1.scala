d726a8588f611ed6b112420136f24d1e7f3e8304
Tony Morris <tmorris@tmorris.net>
2012-12-21 12:32:52 +1000

    Association for Grouped

---
+++ src/main/scala/com/nicta/scoobi/core/Association1.scala
@@ -0,0 +1,94 @@
+package com.nicta.scoobi
+package core
+
+import impl.collection.Iterable1
+import scalaz._, Scalaz._
+
+sealed trait Association1[+K, +V] {
+  val key: K
+  val values: Iterable1[V]
+
+  def map[W](f: V => W): Association1[K, W] =
+    Association1(key, values map f)
+
+  def :->[W](f: V => W): Association1[K, W] =
+    map(f)
+
+  def foreach[W](f: V => Unit) =
+    values foreach f
+
+  def mapKey[L](f: K => L): Association1[L, V] =
+    Association1(f(key), values)
+
+  def <-:[L](f: K => L): Association1[L, V] =
+    mapKey(f)
+
+  def bimap[W, L](f: K => L, g: V => W): Association1[L, W] =
+    Association1(f(key), values map g)
+
+  def product[KK >: K, W](w: Association1[KK, W])(implicit S: Semigroup[KK]): Association1[KK, (V, W)] =
+    Association1(S.append(key, w.key), values zip w.values)
+
+  def ***[KK >: K, W](w: Association1[KK, W])(implicit S: Semigroup[KK]): Association1[KK, (V, W)] =
+    product(w)
+
+  def ap[KK >: K, W](f: Association1[KK, V => W])(implicit S: Semigroup[KK]): Association1[KK, W] =
+    Association1(S.append(key, f.key), values ap f.values)
+
+  def <*>:[KK >: K, W](f: Association1[KK, V => W])(implicit S: Semigroup[KK]): Association1[KK, W] =
+    ap(f)
+
+  def firstValue: V =
+    values.head
+
+  def traverseKey[F[_]: Applicative, L, VV >: V](f: K => F[L]): F[Association1[L, VV]] =
+    implicitly[Functor[F]].map(f(key))(l => Association1(l, values))
+
+}
+
+object Association1 {
+  def apply[K, V](k: K, vs: Iterable1[V]): Association1[K, V] =
+    new Association1[K, V] {
+      val key =
+        k
+      val values =
+        vs
+    }
+
+  def keyL[K, V]: Association1[K, V] @> K =
+    Lens(a => Store(Association1(_, a.values), a.key))
+
+  def valuesL[K, V]: Association1[K, V] @> Iterable1[V] =
+    Lens(a => Store(Association1(a.key, _), a.values))
+
+  def firstValueL[K, V]: Association1[K, V] @> V =
+    valuesL >=> Iterable1.headL
+
+  implicit def Associative1Functor[K]: Functor[({type λ[α] = Association1[K, α]})#λ] =
+    new Functor[({type λ[α] = Association1[K, α]})#λ] {
+      def map[A, B](a: Association1[K, A])(f: A => B) =
+        a map f
+    }
+
+  implicit def Associative1Zip[K: Semigroup]: Zip[({type λ[α] = Association1[K, α]})#λ] =
+    new Zip[({type λ[α] = Association1[K, α]})#λ] {
+      def zip[A, B](a: => Association1[K, A], b: => Association1[K, B]) =
+        a product b
+    }
+
+  implicit def Associative1ApplyZip[K: Semigroup]: Zip[({type λ[α] = Association1[K, α]})#λ] with Apply[({type λ[α] = Association1[K, α]})#λ] =
+    new Zip[({type λ[α] = Association1[K, α]})#λ] with Apply[({type λ[α] = Association1[K, α]})#λ] {
+      override def map[A, B](a: Association1[K, A])(f: A => B) =
+        a map f
+      override def zip[A, B](a: => Association1[K, A], b: => Association1[K, B]) =
+        a product b
+      def ap[A, B](a: => Association1[K, A])(f: => Association1[K, A => B]) =
+        a ap f
+    }
+
+  implicit val Associative1Bifunctor: Bifunctor[Association1] =
+    new Bifunctor[Association1] {
+      def bimap[A, B, C, D](a: Association1[A, B])(f: A => C, g: B => D): Association1[C, D] =
+        a bimap (f, g)
+    }
+}
--- src/main/scala/com/nicta/scoobi/core/Grouped.scala
+++ src/main/scala/com/nicta/scoobi/core/Grouped.scala
@@ -6,16 +6,34 @@ import impl.collection.Iterable1
 sealed trait Grouped[K, V] {
   val list: DList[(K, Iterable1[V])]
 
-  //   def map[B: Manifest : WireFormat](f: A => B): DList[B] =
+  def map[W](f: V => W)(implicit MK: Manifest[K], FK: WireFormat[K], MW: Manifest[W], FW: WireFormat[W]): Grouped[K, W] =
+    mapValues(_ map f)
 
-  def map[W](f: V => W)(implicit MK: Manifest[K], FK: WireFormat[K], MW: Manifest[W], FW: WireFormat[W]): Grouped[K, W] = {
-    val g = implicitly[Manifest[(K, Iterable1[W])]]
-    val h = implicitly[WireFormat[Iterable[W]]]
-    // val h = implicitly[WireFormat[(K, Iterable1[W])]]
-    Grouped(list.map(error(""))(g, error("")))
-  }
+  // foreach
 
-  // (implicit MK: Manifest[K], FK: WireFormat[K], MV: Manifest[V], FV: WireFormat[V])
+  def :->[W](f: V => W)(implicit MK: Manifest[K], FK: WireFormat[K], MW: Manifest[W], FW: WireFormat[W]): Grouped[K, W] =
+    map(f)
+
+  def mapKeys[L](f: K => L)(implicit ML: Manifest[L], FL: WireFormat[L], MV: Manifest[V], FV: WireFormat[V]): Grouped[L, V] =
+    Grouped(list map {
+      case (k, vs) => (f(k), vs)
+    })
+
+  def <-:[L](f: K => L)(implicit ML: Manifest[L], FL: WireFormat[L], MV: Manifest[V], FV: WireFormat[V]): Grouped[L, V] =
+    mapKeys(f)
+
+  def mapValues[W](f: Iterable1[V] => Iterable1[W])(implicit MK: Manifest[K], FK: WireFormat[K], MW: Manifest[W], FW: WireFormat[W]): Grouped[K, W] =
+    Grouped(list map {
+      case (k, vs) => (k, f(vs))
+    })
+
+  // bimap, product, ap
+
+  def keys(implicit MK: Manifest[K], FK: WireFormat[K]): DList[K] =
+    list map (_._1)
+
+  def values(implicit MW: Manifest[V], FW: WireFormat[V]) =
+    list map (_._2)
 }
 
 object Grouped {
