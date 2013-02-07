4d3db94a4d5e5d5e11051d28fc70a4b651fa38c3
Tony Morris <tmorris@tmorris.net>
2013-02-07 11:40:32 +1000

    Iterator1 alias

--- src/main/scala/com/nicta/scoobi/Scoobi.scala
+++ src/main/scala/com/nicta/scoobi/Scoobi.scala
@@ -33,7 +33,7 @@ object Scoobi extends core.WireFormatImplicits with core.GroupingImplicits with 
 
   type Grouped1[K, V] = com.nicta.scoobi.core.Grouped1[K, V]
   type Iterable1[A] = com.nicta.scoobi.core.Iterable1[A]
-  type Iteraor1[A] = com.nicta.scoobi.core.Iterator1[A]
+  type Iterator1[A] = com.nicta.scoobi.core.Iterator1[A]
   type Association1[K, V] = com.nicta.scoobi.core.Association1[K, V]
   type DoFn[A, B] = com.nicta.scoobi.core.DoFn[A, B]
   type BasicDoFn[A, B] = com.nicta.scoobi.core.BasicDoFn[A, B]
