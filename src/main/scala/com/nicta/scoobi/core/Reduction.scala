e1740b6206b959cccf728c76ca9bd036a0485794
Tony Morris <tmorris@tmorris.net>
2013-02-07 16:31:38 +1000

    reduction (semigroup)

--- src/main/scala/com/nicta/scoobi/core/Reduction.scala
+++ src/main/scala/com/nicta/scoobi/core/Reduction.scala
@@ -1,8 +1,22 @@
 package com.nicta.scoobi
 package core
 
+import scalaz._
+
 trait Reduction[A] {
   val reduce: (A, A) => A
+
+  def reduceC: A => A => A =
+    reduce.curried
+
+  def dual: Reduction[A] =
+    Reduction((a1, a2) => reduce(a2, a1))
+
+  def unary_~ : Reduction[A] =
+    dual
+
+  def pair: A => A =
+    a => reduce(a, a)
 
   def zip[B](r: Reduction[B]): Reduction[(A, B)] =
     Reduction {
@@ -12,6 +26,35 @@ trait Reduction[A] {
   def ***[B](r: Reduction[B]): Reduction[(A, B)] =
     zip(r)
 
+  def left[B](r: => Reduction[B]): Reduction[A \/ B] =
+    Reduction((x1, x2) => x1 match {
+      case -\/(a1) => x2 match {
+        case -\/(a2) => -\/(reduce(a1, a2))
+        case \/-(_) => -\/(a1)
+      }
+      case \/-(b1) => x2 match {
+        case -\/(_) => \/-(b1)
+        case \/-(b2) => \/-(r reduce (b1, b2))
+      }
+    })
+
+  def right[B](r: => Reduction[B]): Reduction[A \/ B] =
+    Reduction((x1, x2) => x1 match {
+      case -\/(a1) => x2 match {
+        case -\/(a2) => -\/(reduce(a1, a2))
+        case \/-(b1) => \/-(b1)
+      }
+      case \/-(b1) => x2 match {
+        case -\/(a1) => -\/(a1)
+        case \/-(b2) => \/-(r reduce (b1, b2))
+      }
+    })
+
+  def xmap[B](f: A => B, g: B => A): Reduction[B] =
+    Reduction((b1, b2) => f(reduce(g(b1), g(b2))))
+
+  def semigroup: Semigroup[A] =
+    Semigroup.instance((a1, a2) => reduce(a1, a2))
 }
 
 object Reduction {
@@ -22,4 +65,11 @@ object Reduction {
 
   def constant[A](a: => A): Reduction[A] =
     apply((_, _) => a)
+
+  def split1[A](f: A => A): Reduction[A] =
+    apply((a1, _) => f(a1))
+
+  def split2[A](f: A => A): Reduction[A] =
+    apply((_, a2) => f(a2))
+
 }
